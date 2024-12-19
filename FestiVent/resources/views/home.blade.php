05e6c45c4a8a1e94190ea537bb404cbbfb5c0860
yarfth <148038393+yarfth@users.noreply.github.com>
2024-12-19 14:24:55 +0700

    Update paling baru

--- FestiVent/resources/views/home.blade.php
+++ FestiVent/resources/views/home.blade.php
@@ -161,6 +161,7 @@
           <div class="col-lg-3 col-md-6 d-flex justify-content-center">
             <div class="stats-item text-center w-100 h-100">
               <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
+              
               <p>Community</p>
             </div>
           </div><!-- End Stats Item -->
