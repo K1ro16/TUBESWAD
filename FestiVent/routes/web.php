b7dac898cdd88fdcb013c133f282e3d66981a6a5
Khalll75 <Khalishaathaya04@gmail.com>
2024-12-21 10:17:18 +0700

    benar2

--- FestiVent/routes/web.php
+++ FestiVent/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RequestController;
+use App\Http\Controllers\EventReqController;
 use App\Http\Controllers\AccountsController;
 
 Route::get('/', function () {
@@ -34,14 +34,12 @@ Route::post('/accounts/login', [AccountsController::class, 'login'])->name('acco
 Route::post('/accounts/logout', [AccountsController::class, 'logout'])->name('accounts.logout');
 
 // Menambahkan rute untuk Requests
-Route::prefix('requests')->name('requests.')->group(function () {
-    Route::get('/', [RequestController::class, 'index'])->name('index');
-    Route::get('/create', [RequestController::class, 'create'])->name('create');
-    Route::post('/', [RequestController::class, 'store'])->name('store');
-    Route::get('/{id}/edit', [RequestController::class, 'edit'])->name('edit');
-    Route::put('/{id}', [RequestController::class, 'update'])->name('update');
-    Route::delete('/{id}', [RequestController::class, 'destroy'])->name('destroy');
-});
-
+Route::get('/eventreq', [EventReqController::class, 'index'])->name('eventreq.index'); // Show event list
+Route::get('/eventreq/create', [EventReqController::class, 'create'])->name('eventreq.create'); // Show form to create new event
+Route::post('/eventreq', [EventReqController::class, 'store'])->name('eventreq.store'); // Store new event
+Route::get('/eventreq/{id}', [EventReqController::class, 'show'])->name('eventreq.show'); // Show event details
+Route::get('/eventreq/{id}/edit', [EventReqController::class, 'edit'])->name('eventreq.edit'); // Show edit form for event
+Route::put('/eventreq/{id}', [EventReqController::class, 'update'])->name('eventreq.update'); // Update event
+Route::delete('/eventreq/{id}', [EventReqController::class, 'destroy'])->name('eventreq.destroy'); // Delete event
 
 
